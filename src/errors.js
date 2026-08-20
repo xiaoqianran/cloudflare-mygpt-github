178ca1c174e263b8cc0d434dfd2ee97c5adc14f2
xiaoqianran <1416220530@qq.com>
2026-08-21 01:27:35 +0800

    refactor: add shared HTTP error type

---
+++ src/errors.js
@@ -0,0 +1,8 @@
+export class HttpError extends Error {
+  constructor(status, message, details) {
+    super(message);
+    this.name = "HttpError";
+    this.status = status;
+    this.details = details;
+  }
+}
